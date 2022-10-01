f49e54b8ba8e05ee6395592ae24ce17b55f34a51
Ilson Warmling <ilsonwarsc@gmail.com>
2022-10-01 20:40:34 -0300

    Subtotal & Total methods

--- src/main/java/com/coursenelio/course/entities/OrderItem.java
+++ src/main/java/com/coursenelio/course/entities/OrderItem.java
@@ -13,10 +13,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
-
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-
+	
 	private Integer quantity;
 	private Double price;
 	
@@ -29,7 +28,6 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
@@ -52,5 +50,39 @@ public class OrderItem implements Serializable {
 	}
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
